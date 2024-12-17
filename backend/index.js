11b472074c65842229a66b63b15f9e274a5b3033
Anubhav Das <anubhavdas@Anubhavs-MacBook-Air.local>
2024-12-17 23:32:58 +0530

    last commit

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://job-finder-vj0e.onrender.com',
   credentials: true,
 };
 app.use(cors(corsOptions));
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_ENDPOINT = "http://localhost:7018/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:7018/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:7018/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:7018/api/company";
+export const USER_API_ENDPOINT = "https://job-finder-vj0e.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://job-finder-vj0e.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://job-finder-vj0e.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://job-finder-vj0e.onrender.com/api/company";
 
